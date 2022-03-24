31f038b3423c42ed53515bf4adb5f5ca3aeb847e
CG-SKYLN <70344561+CG-SKYLN@users.noreply.github.com>
2022-03-24 17:23:59 -0600

    Add files via upload

--- src/Writer.java
+++ src/Writer.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.nio.charset.*;
 import java.nio.file.*;
 
-public class Thing {
+class Writer {
 	public static void main(String[] args) {
 		System.out.println(System.getProperty("os.name"));
 		WriteALot np = new WriteALot ();
@@ -14,13 +14,14 @@ public class Thing {
 class WriteALot implements ActionListener {
 	JFrame frm;
 	JMenuBar mnubr;
-	TogMenu fileMenu, editMenu, helpMenu;
+	JMenu fileMenu, editMenu, helpMenu;
 	JMenuItem cutItem, copyItem, pasteItem, selectAll, saveFile, saveFileAs, openFile;
 	JTextArea txtarea;
 
 	WriteALot() {
 		frm = new JFrame();
-		frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frm.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+	frm.setSize(500, 500);
 		cutItem = new JMenuItem("Cut");
 		copyItem = new JMenuItem("Copy");
 		pasteItem = new JMenuItem("Paste");
@@ -39,9 +40,9 @@ class WriteALot implements ActionListener {
 		saveFileAs.addActionListener(this);
 		openFile.addActionListener(this);
 		mnubr = new JMenuBar();
-		mnubr.setBounds(5, 5, 400, 40);
-		fileMenu = new TogMenu("File");
-		editMenu = new TogMenu("Edit");
+		mnubr.setBounds(5, 0, (frm.getWidth()-5), 10);
+		fileMenu = new JMenu("File");
+		editMenu = new JMenu("Edit");
 
 		fileMenu.add(openFile);
 		fileMenu.add(saveFile);
@@ -53,17 +54,47 @@ class WriteALot implements ActionListener {
 		editMenu.add(selectAll);
 		mnubr.add(fileMenu);
 		mnubr.add(editMenu);
-		mnubr.add(helpMenu);
+		frm.addWindowListener(new WindowAdapter() {
+
+			@Override
+			public void windowClosing(WindowEvent we)
+			{ 
+				String ObjButtons[] = {"Yes","No"};
+				int PromptResult = JOptionPane.showOptionDialog(null, 
+						"Are you sure you want to exit?", "Online Examination System", 
+						JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE, null, 
+						ObjButtons,ObjButtons[1]);
+				if(PromptResult==0)
+				{
+					System.exit(0);          
+				}
+			}
+		});
 		txtarea = new JTextArea();
-		txtarea.setBounds(5, 30, 460, 460);
+		txtarea.setBounds(0, 11, frm.getWidth(), frm.getHeight());
 		frm.add(mnubr);
 		frm.add(txtarea);
-		frm.setLayout(null);
-		frm.setSize(500, 500);
 		frm.setVisible(true);
+		while(1==1){
+		display();
+		}
+	}
+	
+	public void display(){
+		txtarea.setBounds(0, 50, frm.getWidth(), frm.getHeight());
+mnubr.setBounds(5, 50, (frm.getWidth()-5), 50);
 	}
 
 	public void actionPerformed(ActionEvent ae) {
+		String act = ae.getActionCommand();
+		if(act =="click"&& (ae.getSource() == fileMenu || ae.getSource()==editMenu)){
+			JPopupMenu pm = new JPopupMenu("Options");
+			pm.add(cutItem);
+			pm.add(saveFile);
+			pm.show(frm,50,50);
+		}
+
+
 		if (ae.getSource() == cutItem) {
 			txtarea.cut();
 		} else if (ae.getSource() == pasteItem) {
@@ -76,30 +107,29 @@ class WriteALot implements ActionListener {
 			try {
 				FileManage.save("testFile", txtarea);
 			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		} else if (ae.getSource() == saveFileAs) {
 
 		} else if (ae.getSource() == openFile) {
 
-		}
+		} 
 	}
+
 
 }
 
 class TogMenu extends JMenu implements Toggleable{
 	boolean toggled=false;	
-	
+
 	TogMenu(String s){
 		super(s);
 	}
-	
+
 	public void toggleThis() {
-		
+		this.setVisible((toggled==true)?false:true);	
 	}
 }
 
@@ -107,13 +137,13 @@ abstract class FileManage {
 	public static void save(String nm, JTextArea tx) throws IOException, FileNotFoundException {
 
 		final String content = tx.getText();
-	    final Path path = Paths.get(System.getProperty("user.home")+"/Downloads"+nm+".txt");
+		final Path path = Paths.get(System.getProperty("user.home")+"/Downloads"+nm+".txt");
 		try (
-		        final BufferedWriter writer = Files.newBufferedWriter(path,
-		            StandardCharsets.UTF_8, StandardOpenOption.CREATE);
+				final BufferedWriter writer = Files.newBufferedWriter(path,
+					StandardCharsets.UTF_8, StandardOpenOption.CREATE);
 		    ) {
-		        writer.write(content);
-		        writer.flush();
+			writer.write(content);
+			writer.flush();
 		    }
 		catch(FileNotFoundException e) {
 			File f = new File(path.toString());
@@ -124,15 +154,15 @@ abstract class FileManage {
 		File f = new File(System.getProperty("user.home") + "/Downloads/" + nm + ".txt");
 	}
 	public static void open(String nm) throws IOException, FileNotFoundException{
-		
-		
-		
-		
+
+
+
+
 	}
-	
+
 }
 
 interface Toggleable{
 	boolean toggled = false;
 	public void toggleThis();
-	}
+}
